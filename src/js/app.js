b51533995b295120d07f1797ed663855a109df94
Anastasia Kupriyanova <anastasy.kuprianova@gmail.com>
2016-06-07 20:39:58 +0300

    Added integration with react router.

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, combineReducers} from 'redux';
+import {Router, Route, browserHistory} from 'react-router';
+import {syncHistoryWithStore, routerReducer} from 'react-router-redux';
 import {Provider} from 'react-redux';
 import {Grid} from 'react-bootstrap';
 
@@ -9,31 +11,45 @@ import Header from './shared/containers/header';
 import Footer from './shared/components/footer';
 
 
-function reducer(state = {}, action) {
+function reducer(state = {user: null}, action) {
     switch (action.type) {
     default:
-        return state;
+        break;
     }
+
+    return state;
 }
 
 const store = (window.devToolsExtension ?
                window.devToolsExtension()(createStore) :
-               createStore)(reducer);
+               createStore)(combineReducers({
+                   reducer,
+                   routing: routerReducer
+               }));
 
 store.subscribe(() => console.log(store.getState()));
 
-const App = () => (
+const App = ({children}) => (
     <Grid>
         <Header />
-        <Posts />
+        {children}
         <hr />
         <Footer />
     </Grid>
 );
 
+App.propTypes = {
+    children: React.PropTypes.element.isRequired
+};
+
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router history={syncHistoryWithStore(browserHistory, store)}>
+            <Route component={App}>
+                <Route path="/" component={Posts} />
+                <Route path="posts" component={Posts} />
+            </Route>
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
