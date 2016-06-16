386dd7662daa355216c42ddb671b1ce405a0934e
Anastasia Kupriyanova <anastasy.kuprianova@gmail.com>
2016-06-16 20:40:16 +0300

    Minor cleanup in constants.

--- src/js/membership/constants.js
+++ src/js/membership/constants.js
@@ -1,7 +1,11 @@
-export const SIGN_IN_REQUEST = 'SIGN_IN_REQUEST';
-export const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
-export const SIGN_IN_FAILURE = 'SIGN_IN_FAILURE';
+export const SIGN_IN_REQUEST = 'membership/SIGN_IN_REQUEST';
+export const SIGN_IN_SUCCESS = 'membership/SIGN_IN_SUCCESS';
+export const SIGN_IN_FAILURE = 'membership/SIGN_IN_FAILURE';
 
-export const SIGN_OUT_REQUEST = 'SIGN_OUT_REQUEST';
-export const SIGN_OUT_SUCCESS = 'SIGN_OUT_SUCCESS';
-export const SIGN_OUT_FAILURE = 'SIGN_OUT_FAILURE';
+export const SIGN_UP_REQUEST = 'membership/SIGN_UP_REQUEST';
+export const SIGN_UP_SUCCESS = 'membership/SIGN_UP_SUCCESS';
+export const SIGN_UP_FAILURE = 'membership/SIGN_UP_FAILURE';
+
+export const SIGN_OUT_REQUEST = 'membership/SIGN_OUT_REQUEST';
+export const SIGN_OUT_SUCCESS = 'membership/SIGN_OUT_SUCCESS';
+export const SIGN_OUT_FAILURE = 'membership/SIGN_OUT_FAILURE';
