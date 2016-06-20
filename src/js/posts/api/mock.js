3e39e10556df018605b5d9360469ff87a12b6499
Anastasia Kupriyanova <anastasy.kuprianova@gmail.com>
2016-06-20 20:33:08 +0300

    Introduced posts mock API.

---
+++ src/js/posts/api/mock.js
@@ -0,0 +1,76 @@
+import * as _ from '../../shared/mock';
+import samples from 'json!./samples.json';
+import msamples from 'json!../../membership/api/samples.json';
+
+
+const findUserById = id => _.first(msamples.users, u => u.id === id);
+
+export default {
+    searchPosts: (q = null, page = 0) => {
+        var posts = samples.posts;
+
+        if (q) {
+            q = q.toLowerCase();
+            posts = _.nfilter(
+                posts,
+                (page + 1) * 2 + 1,
+                p => p.title.toLowerCase().indexOf(q) > -1);
+        }
+
+        return _.resolve(_.pager(posts, page, 2, p => {
+            var a = findUserById(p.author_id);
+
+            return {
+                slug: p.slug,
+                title: p.title,
+                author: {
+                    first_name: a.first_name, last_name: a.last_name
+                },
+                created_on: p.created_on,
+                message: _.trancateWords(p.message, 40)
+            };
+        }));
+    },
+
+    getPost: slug => {
+        const p = _.first(samples.posts, p => p.slug === slug);
+
+        if (!p) {
+            return _.reject();
+        }
+
+        const a = findUserById(p.author_id);
+
+        return _.resolve({
+            slug: p.slug,
+            title: p.title,
+            created_on: p.created_on,
+            author: {
+                first_name: a.first_name, last_name: a.last_name
+            },
+            message: p.message,
+            permissions: {
+                create_comment: p.permissions && p.permissions.create_comment
+            },
+            comments: samples.comments.filter(c => c.post_id === p.id).map(
+                c => {
+                    const ca = findUserById(c.author_id);
+
+                    return {
+                        author: {
+                            first_name: ca.first_name,
+                            last_name: ca.last_name,
+                            gravatar_hash: ca.gravatar_hash
+                        },
+                        created_on: c.created_on,
+                        message: c.message,
+                        moderated: c.moderated
+                    };
+                })
+        });
+    }
+};
+
+/*
+ eslint camelcase:0
+*/
--- src/js/shared/mock.js
+++ src/js/shared/mock.js
@@ -4,6 +4,15 @@ export const resolve = (r, timeout) => new Promise(resolve =>
 export const reject = (r, timeout) => new Promise((resolve, reject) =>
     setTimeout(() => reject(r), timeout || 100));
 
+export const trancateWords = (s, count) => {
+    s = s.split(/\s|\\n/).slice(0, count);
+    if (s.length === count) {
+        s.push('...');
+    }
+
+    return s.join(' ');
+};
+
 export const first = (items, predicate) => {
     for (let d of items) {
         if (predicate(d)) {
@@ -12,4 +21,44 @@ export const first = (items, predicate) => {
     }
 
     return null;
+};
+
+export const nfilter = (items, n, predicate) => {
+    var r = [];
+
+    for (let d of items) {
+        if (predicate(d)) {
+            r.push(d);
+            n -= 1;
+
+            if (!n) {
+                break;
+            }
+        }
+    }
+
+    return r;
+};
+
+export const pager = (items, page, size, f) => {
+    var start = page * size,
+        end = start + size,
+        paging = {},
+        r = [];
+
+    if (page > 0) {
+        paging.before = page - 1;
+    }
+
+    if (end < items.length) {
+        paging.after = page + 1;
+    } else {
+        end = items.length;
+    }
+
+    for (var i = start; i < end; i++) {
+        r.push(f(items[i]));
+    }
+
+    return {paging: paging, items: r};
 };
