3a9086d87e41bd53e08252b6f3f62f8b83090924
Anastasia Kupriyanova <anastasy.kuprianova@gmail.com>
2016-07-07 20:13:37 +0300

    Introduced add post comment constants.

--- src/js/posts/constants.js
+++ src/js/posts/constants.js
@@ -5,3 +5,7 @@ export const POSTS_FAILURE = 'posts/POSTS_FAILURE';
 export const POST_REQUEST = 'posts/POST_REQUEST';
 export const POST_SUCCESS = 'posts/POST_SUCCESS';
 export const POST_FAILURE = 'posts/POST_FAILURE';
+
+export const ADD_COMMENT_REQUEST = 'posts/ADD_COMMENT_REQUEST';
+export const ADD_COMMENT_SUCCESS = 'posts/ADD_COMMENT_SUCCESS';
+export const ADD_COMMENT_FAILURE = 'posts/ADD_COMMENT_FAILURE';
