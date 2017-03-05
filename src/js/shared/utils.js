95d021c398c618ef91a741225bfd2fb9a996835f
Anastasia Kupriyanova <anastasy.kuprianova@gmail.com>
2017-03-05 20:51:30 +0200

    Migrated from mementjs to date-fns.

--- src/js/shared/utils.js
+++ src/js/shared/utils.js
@@ -1,13 +1,14 @@
-import moment from 'moment';
+import parse from 'date-fns/parse';
+import differenceInDays from 'date-fns/difference_in_days';
+import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
+import format from 'date-fns/format';
 
 
 export const formatDateOrTime = d => {
-    d = moment(d, moment.ISO_8601, true);
-    var t = moment.duration(d - moment());
-
-    if (t.asDays() >= -3) {
-        return t.humanize(true);
+    d = parse(d);
+    if (differenceInDays(d, new Date()) >= -3) {
+        return distanceInWordsToNow(d);
     }
 
-    return d.utc().format('[on] MMMM DD, YYYY [at] hh:mm A');
+    return format(d, '[on] MMMM DD, YYYY [at] hh:mm A');
 };
