76116e33c88c0d54d864ea2baf6ece9431e48088
Anastasia Kupriyanova <anastasy.kuprianova@gmail.com>
2017-03-06 20:54:52 +0200

    Introduced membership actions spec for signin.

---
+++ src/js/membership/actions.spec.js
@@ -0,0 +1,37 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import * as actions from './actions';
+import * as types from './constants';
+
+
+const middlewares = [ thunk ];
+const mockStore = configureMockStore(middlewares);
+
+
+describe('membership actions', () => {
+    describe('signin', () => {
+        it('responds with validation error', () => {
+            const store = mockStore({});
+
+            return store.dispatch(actions.signin()).then(() => {
+                expect(store.getActions()).toEqual([
+                    {
+                        type: types.SIGN_IN_REQUEST
+                    },
+                    {
+                        type: types.SIGN_IN_FAILURE,
+                        errors: {
+                            password: [
+                                'Required field can not be left bank.'
+                            ],
+                            username: [
+                                'Required field can not be left bank.'
+                            ]
+                        }
+                    }
+                ]);
+            });
+        });
+    });
+});
