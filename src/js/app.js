4f3e0a09ff770974a809f7c2ccd0fd2592fbcd9a
Anastasia Kupriyanova <anastasy.kuprianova@gmail.com>
2017-07-15 20:38:24 +0300

    Use router basename as window location pathname.

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, compose, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import {Router, browserHistory} from 'react-router';
+import {Router, useRouterHistory} from 'react-router';
+import {createHistory} from 'history';
 import {routerMiddleware, syncHistoryWithStore} from 'react-router-redux';
 import {Provider} from 'react-redux';
 
@@ -12,8 +13,12 @@ import {dailyQuote} from './shared/actions';
 
 import './app.scss';
 
+const history = useRouterHistory(createHistory)({
+    basename: window.location.pathname
+});
+
 const enhancer = applyMiddleware(
-    routerMiddleware(browserHistory),
+    routerMiddleware(history),
     thunk);
 const store = createStore(reducers, {},
     window.devToolsExtension ?
@@ -24,7 +29,7 @@ store.dispatch(dailyQuote());
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={syncHistoryWithStore(browserHistory, store)}>
+        <Router history={syncHistoryWithStore(history, store)}>
             {routes}
         </Router>
     </Provider>,
